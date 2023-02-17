b94ef4c276fa54e3d134b5fc5d13690e3b86d5fa
g1alexander <alexander.granados.dev@gmail.com>
2023-02-17 11:25:31 -0500

    feat: config alias vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@components": fileURLToPath(
+        new URL("./src/components", import.meta.url)
+      ),
+      "@pages": fileURLToPath(new URL("./src/pages", import.meta.url)),
+      "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+      "@styles": fileURLToPath(new URL("./src/styles", import.meta.url)),
+      "@helpers": fileURLToPath(new URL("./src/helpers", import.meta.url)),
+      "@router": fileURLToPath(new URL("./src/router", import.meta.url)),
+      "@store": fileURLToPath(new URL("./src/store", import.meta.url)),
+    },
+  },
+});
