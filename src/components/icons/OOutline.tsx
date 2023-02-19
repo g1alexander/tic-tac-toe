a374746a4abf8c9811004093ff8d814043e1179a
g1alexander <alexander.granados.dev@gmail.com>
2023-02-18 21:24:23 -0500

    feat: components icons outline

---
+++ src/components/icons/OOutline.tsx
@@ -0,0 +1,21 @@
+function OOutline({ state }: { state: boolean }) {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M32 16C32 7.16344 24.8366 0 16 0C7.16344 0 0 7.16344 0 16C0 24.8366 7.16344 32 16 32C24.8366 32 32 24.8366 32 16ZM9.48145 16C9.48145 12.3999 12.3999 9.48147 16 9.48147C19.6 9.48147 22.5185 12.3999 22.5185 16C22.5185 19.6001 19.6 22.5185 16 22.5185C12.3999 22.5185 9.48145 19.6001 9.48145 16Z"
+        stroke-width="2"
+        stroke={!state ? "#A8BFC9" : "#1A2A33"}
+        fill={state ? "#A8BFC9" : "#1A2A33"}
+      />
+    </svg>
+  );
+}
+
+export default OOutline;
