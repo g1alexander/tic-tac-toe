4351beab6a08993d07bf8f956ba0aa7f8753a490
g1alexander <alexander.granados.dev@gmail.com>
2023-02-20 14:44:20 -0500

    feat: view home complete

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,49 @@
-import IconO from "@assets/icon-o.svg";
-import IconX from "@assets/icon-x.svg";
 import OOutline from "@components/icons/OOutline";
 import XOutline from "@components/icons/XOutline";
+import Btn from "@components/shared/Btn";
+import Logo from "@components/shared/Logo";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+
+  const handleClick = (typeGame: string) =>
+    navigate("/game", { state: { typeGame } });
+
   return (
     <section className="h-screen flex flex-col items-center justify-center gap-10">
-      <article className="flex gap-2">
-        <img src={IconX} alt="icon-o" className="w-10 h-10" />
-        <img src={IconO} alt="icon-x" className="w-10 h-10" />
-      </article>
+      <Logo width={10} height={10} />
 
       <article className="bg-black-300 w-[90%] rounded-lg p-5 text-center">
         <h1 className="font-bold mb-5 text-lg">PICK PLAYER 1’S MARK</h1>
 
         <article className="bg-black-400 py-3 rounded-lg flex w-full mb-5">
-          <div className="w-1/2 mx-auto flex justify-center items-center">
+          <button className="w-1/2 mx-auto flex justify-center items-center">
             <XOutline state={true} />
-          </div>
-          <div className="bg-gray-400 rounded-lg flex justify-center items-center px-6 py-3 w-1/2 mx-auto mr-3">
+          </button>
+          <button className="bg-gray-400 rounded-lg flex justify-center items-center px-6 py-3 w-1/2 mx-auto mr-3">
             <OOutline state={false} />
-          </div>
+          </button>
         </article>
 
         <h3 className="text-gray-500">REMEMBER : X GOES FIRST</h3>
       </article>
 
-      <article>{/* btns */}</article>
+      <article className="flex flex-col gap-3 w-[90%]">
+        <div
+          onClick={() => handleClick("cpu")}
+          className="w-full bg-yellow-500 rounded-2xl pb-2"
+        >
+          <Btn color="bg-yellow-400">new game (vs cpu)</Btn>
+        </div>
+
+        <div
+          onClick={() => handleClick("player")}
+          className="w-full bg-blue-500 rounded-2xl pb-2"
+        >
+          <Btn color="bg-blue-400"> new Game (vs player)</Btn>
+        </div>
+      </article>
     </section>
   );
 }
