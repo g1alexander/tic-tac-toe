fe57fb71c248f7c37d70a0173e8bd843987aaf7a
g1alexander <alexander.granados.dev@gmail.com>
2023-02-23 11:26:06 -0500

    feat: modal-provider in root app

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "@router/index";
+import { ModalProvider } from "@context/ContextModal";
 
 import "@styles/global.css";
 import "@styles/tailwind.css";
@@ -9,7 +10,9 @@ import "@styles/tailwind.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <main className="bg-black-400 text-gray-400 h-screen">
-      <RouterProvider router={router} />
+      <ModalProvider>
+        <RouterProvider router={router} />
+      </ModalProvider>
     </main>
   </React.StrictMode>
 );
