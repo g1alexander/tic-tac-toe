ed3fd37d7447c13271391efc1fa32b74b87f3337
g1alexander <alexander.granados.dev@gmail.com>
2023-02-24 12:52:49 -0500

    feat: data dynamic

--- src/components/game/Score.tsx
+++ src/components/game/Score.tsx
@@ -1,21 +1,32 @@
 import Btn from "@components/shared/Btn";
+import { useGame } from "@hooks/useGame";
 
 function Score() {
+  const { pickPlayer, score, typeGame } = useGame();
+
+  const handleCPU = (isBlue: boolean) => {
+    if (typeGame === "player") return isBlue ? "p1" : "p2";
+
+    if (isBlue) return pickPlayer === "X" ? "you" : "cpu";
+
+    return pickPlayer === "O" ? "you" : "cpu";
+  };
+
   return (
     <section className="flex justify-between w-[90%] mx-auto">
       <Btn classCSS="bg-blue-400 rounded-lg px-7">
-        <span className="block uppercase text-xs">x (you)</span>
-        <span className="font-bold block">14</span>
+        <span className="block uppercase text-xs">x ({handleCPU(true)})</span>
+        <span className="font-bold block">{score.playerX}</span>
       </Btn>
 
       <Btn classCSS="bg-gray-400 rounded-lg px-7">
         <span className="block uppercase text-xs">ties</span>
-        <span className="font-bold block">32</span>
+        <span className="font-bold block">{score.ties}</span>
       </Btn>
 
       <Btn classCSS="bg-yellow-400 rounded-lg px-7">
-        <span className="block uppercase text-xs">o (cpu)</span>
-        <span className="font-bold block">11</span>
+        <span className="block uppercase text-xs">o ({handleCPU(false)})</span>
+        <span className="font-bold block">{score.playerO}</span>
       </Btn>
     </section>
   );
