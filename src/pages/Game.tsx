502d6bd5dfeb828c680596522af18a83f9d1c246
g1alexander <alexander.granados.dev@gmail.com>
2023-02-24 11:59:33 -0500

    chore: delete comment

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,28 +1,8 @@
-// import { useLocation } from "react-router-dom";
-
 import Header from "@components/game/Header";
 import Grid from "@components/game/Grid";
 import Score from "@components/game/Score";
 
 function Game() {
-  // const modal = useContext(ModalContext);
-
-  // const handleTest = () => {
-  //   modal?.setModal({
-  //     showModal: true,
-  //     title: "oh no, you lost…",
-  //     message: {
-  //       text: "player 2 wins!",
-  //       img: IconO,
-  //     },
-  //     btnYellow: "restart game?",
-  //     btnGray: "no, cancel",
-  //   });
-  // };
-  // const location = useLocation();
-
-  // console.log(location.state);
-
   return (
     <>
       <Header />
