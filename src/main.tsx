1ea1dfabb14e635a897d7c680c772cc4d5fbd199
g1alexander <alexander.granados.dev@gmail.com>
2023-02-26 20:48:30 -0500

    refactor: routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import router from "@router/index";
+import { BrowserRouter } from "react-router-dom";
+import App from "@router/index";
 import { ModalProvider } from "@context/ContextModal";
 
 import "@styles/global.css";
@@ -10,9 +10,11 @@ import "@styles/tailwind.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <main className="bg-black-400 text-gray-400 h-screen">
-      <ModalProvider>
-        <RouterProvider router={router} />
-      </ModalProvider>
+      <BrowserRouter>
+        <ModalProvider>
+          <App />
+        </ModalProvider>
+      </BrowserRouter>
     </main>
   </React.StrictMode>
 );
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,20 +1,22 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, useRoutes } from "react-router-dom";
 import Home from "@pages/Home";
 import Game from "@pages/Game";
 import ErrorPage from "@pages/Error";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-
-  {
-    path: "/game",
-    element: <Game />,
-    errorElement: <ErrorPage />,
-  },
-]);
+const router = () => {
+  const routes = useRoutes([
+    {
+      path: "/",
+      element: <Home />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/game",
+      element: <Game />,
+      errorElement: <ErrorPage />,
+    },
+  ]);
+  return routes;
+};
 
 export default router;
