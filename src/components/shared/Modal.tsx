81dfc8541d80bb731aab620e08c9a9971f5a3742
g1alexander <alexander.granados.dev@gmail.com>
2023-02-26 19:21:11 -0500

    fix: delete comment

--- src/components/shared/Modal.tsx
+++ src/components/shared/Modal.tsx
@@ -26,51 +26,36 @@ export default function Modal({
                 <div className="mt-3">
                   <div className="mt-2 text-center sm:ml-4 sm:text-left">
                     <h4 className="text-lg uppercase text-center font-medium ">
-                      {/* oh no, you lost… */}
-                      {/* you won! */}
-                      {/* player 1 wins! */}
-                      {/* player 2 wins! */}
                       {title}
                     </h4>
-                    <p className="my-7 uppercase text-2xl flex items-center justify-center gap-2 ">
-                      {/* <img src={IconO} alt="" className="w-7" />
-                      <span className="block text-yellow-400">takes the round</span> */}
-                      {/* <img src={IconX} alt="" className="w-7" />
-                      <span className="block text-blue-400">
-                        takes the round
-                      </span> */}
-                      {/* restart game? */}
-                      {/* round tied */}
+                    {message.text && (
+                      <p className="my-7 uppercase text-2xl flex items-center justify-center gap-2 ">
+                        {message.img ? (
+                          <>
+                            <img src={message.img} alt="" className="w-7" />
+                            <span className={`${message.color} block`}>
+                              {message.text}
+                            </span>
+                          </>
+                        ) : (
+                          message.text
+                        )}
+                      </p>
+                    )}
 
-                      {message.img ? (
-                        <>
-                          <img src={message.img} alt="" className="w-7" />
-                          <span className={`${message.color} block`}>
-                            {message.text}
-                          </span>
-                        </>
-                      ) : (
-                        message.text
-                      )}
-                    </p>
                     <div className="flex justify-center items-center gap-2 mt-3">
-                      {/* w-1/4  */}
                       <div
                         className={`${
                           btnGray === "quit" ? `w-1/4` : `w-auto`
                         } bg-gray-500 rounded-2xl pb-2`}
                       >
                         <Btn classCSS="bg-gray-400 rounded-2xl w-full">
-                          {/* quit */}
-                          {/* no, cancel */}
                           {btnGray}
                         </Btn>
                       </div>
 
                       <div className="w-1/2 lg:w-auto bg-yellow-500 rounded-2xl pb-2">
                         <Btn classCSS="bg-yellow-400 rounded-2xl w-full">
-                          {/* next round */}
-                          {/* yes, restart */}
                           {btnYellow}
                         </Btn>
                       </div>
